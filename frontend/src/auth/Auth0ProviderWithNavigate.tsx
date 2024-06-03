8e41be5f6e3836a9e9c3cef771d371de6771c7e4
ChristoBongende <100066566+bongende@users.noreply.github.com>
2024-06-04 00:25:36 +0200

    configuring the Auth0

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,14 +1,37 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  // I need to really understand how the code bellow work
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
-  return <div></div>;
+  if (!domain || !clientId || !redirectUri)
+    throw new Error("Unable to initialise auth");
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    /* appState contain some auth variables that we need when user get redirected back 
+    to the app where they were before after log in, and User variable will have details
+    about the loged in user
+    */
+    console.log("USER", user);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: redirectUri }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithNavigate;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoute from "./AppRoute";
 import "./global.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoute />
+      <Auth0ProviderWithNavigate>
+        <AppRoute />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
